6b11e0636d99985dc7e8aea2b686b42add49378d
Surojit Paul <surojit99923@gmail.com>
2022-12-04 22:26:46 +0530

    New test add

--- src/test/add.test.js
+++ src/test/add.test.js
@@ -2,4 +2,8 @@ describe('Add 2 number', () => {
     test('1 and 1 should be 2', () => { 
         expect(1+1).toBe(2)
      })
+     test("2 and 1 is 3",()=>{
+        expect(1+2).toBe()
+
+     })
  })
